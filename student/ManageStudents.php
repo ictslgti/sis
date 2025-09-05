6583c26a97dcd7bbe35f4957c4c2a93188106661
sumanan24 <sumanan24@gmail.com>
2025-09-05 15:21:13 +0530

    edit

--- student/ManageStudents.php
+++ student/ManageStudents.php
@@ -2,6 +2,10 @@
 if (session_status() === PHP_SESSION_NONE) {
   session_start();
 }
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../auth.php';
 
